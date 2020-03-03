61284713e9cda9950b02b035c5c2f5f2504f3994
Josh Roth <JRoth@Morrisseyengineering.com>
2020-03-03 17:06:45 -0600

    add room2space separator

---
+++ JR Tools/RoomSep2SpaceSep.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI.Selection;
+
+namespace JR_Tools
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+
+    class RoomSep2SpaceSep : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = revit.Application.ActiveUIDocument;
+            Document doc = revit.Application.ActiveUIDocument.Document;
+
+            Application app = doc.Application;
+            Autodesk.Revit.Creation.Application appcreation = app.Create;
+            Autodesk.Revit.Creation.Document doccreation = doc.Create;
+
+            ElementId viewid = uidoc.ActiveView.Id;
+            View view = doc.GetElement(viewid) as View;
+            FilteredElementCollector coll = new FilteredElementCollector(doc).WherePasses(new ElementClassFilter(typeof(CurveElement)));
+            var rslines = coll.Where(el => el.Category.Name == "<Room Separation>");
+
+            using (Transaction tx = new Transaction(doc, "Change Division Lines"))
+            {
+                if (tx.Start() == TransactionStatus.Started)
+                {
+                    foreach(Element rsline in rslines)
+                    {
+                        SketchPlane skp = (rsline as CurveElement).SketchPlane;
+                        View curview = doc.GetElement((doc.GetElement(rsline.LevelId) as Level).FindAssociatedPlanViewId()) as View;
+                        Curve cclone = (rsline.Location as LocationCurve).Curve.Clone();
+                        CurveArray curvearr = new CurveArray();
+                        curvearr.Append(cclone);
+                        doccreation.NewSpaceBoundaryLines(skp, curvearr, curview);
+
+                    }
+                }
+                tx.Commit();
+            }
+            return Result.Succeeded;
+        }
+            
+    }
+}
